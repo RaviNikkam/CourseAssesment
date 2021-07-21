e9ef0e4d5873a2f649d872e0e05315ef51f401aa
Ravi NIKKAM <nikkam.ravikumar@gmail.com>
2021-07-21 14:24:30 +0000

    Updated enrolment id

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -134,6 +134,6 @@ class Choice(models.Model):
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
 class Submission(models.Model):
-    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    enrollment_id = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
     #Other fields and methods you would like to design
